b2770c59bb2926fd591c2835e7592349b111ccb0
dance2die <sungmkim80@gmail.com>
2019-06-08 18:12:51 -0400

    added footers

--- src/footer.js
+++ src/footer.js
@@ -1,4 +1,15 @@
-const top = "top";
-const bottom = "bottom";
+import { red, blue } from "./button-styles";
 
-export { top, bottom };
+const top = document.createElement("div");
+top.innerHTML = `<strong>top? of footer</strong>`;
+top.style = red;
+
+const bottom = document.createElement("div");
+bottom.innerHTML = `<strong>bottom of footer</strong>`;
+bottom.style = blue;
+
+const footer = document.createElement("footer");
+footer.appendChild(top);
+footer.appendChild(bottom);
+
+export { top, bottom, footer };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import nav from "./nav";
-import { top, bottom } from "./footer";
+import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
 const button = makeButton("Yay! a real button!");
-button.onclick = () => alert("hi!");
+button.style = makeColorStyle("orange");
+button.onclick = () => alert("awesome!");
 document.body.append(button);
+document.body.append(footer);
 
 // console.log(
 //   nav(),
