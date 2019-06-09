ff851fbff6f57b985a2745edfa7a9fd9a355ba97
dance2die <sungmkim80@gmail.com>
2019-06-08 21:16:17 -0400

    added dev & prod webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const webpackMerge = require("webpack-merge");
+const modeConfig = mode => require(`./build-utils/webpack.${mode}`)(mode);
 
-module.exports = ({ mode }) => {
-    console.log(mode);
-
-    return {
-        mode,
-        // entry: "./src/index.js",
-        output: {
-            filename: "bundle.js"
-        },
-        plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
-    };
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
+  return webpackMerge(
+    {
+      mode,
+      output: {
+        filename: "bundle.js"
+      },
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
+    },
+    modeConfig(mode)
+  );
 };
